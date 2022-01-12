a98a8d5851b91dffa1a1c572dc8a69f684a9f0e1
newkni <47529188+newkni@users.noreply.github.com>
2022-01-13 00:26:29 +0800

    first

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <ListView></ListView>
+  </div>
+</template>
+
+<script>
+import ListView from './components/List.vue'
+export default {
+  name: 'app',
+  components: {
+    ListView
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/List.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="view">
+    <h1 class="title">{{ title }}</h1>
+
+    <ul>
+      <li v-for='item in items' :class="{finish:item.isFinished}" :key="item.text">
+        {{ item.text }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "List",
+  // data:{}
+  // data: function(){ return {}}
+  data() {
+    return {
+      title: "TodoList", // 標題
+      items: [
+        // 代辦事項
+        {
+          text: "練習Vue",
+          isFinished: false,
+        },
+        {
+          text: "練習 Docker",
+          isFinished: true,
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style>
+.view {
+  width: 400px;
+  border: 1px solid gray;
+  margin: 20px auto;
+}
+.view .title{
+    border-bottom: 1px solid gray;
+}
+.view ul li{
+    margin: 10px 0;
+    list-style: none;
+}
+.view .finish{
+    color: gray;
+    text-decoration: line-through;
+}
+</style>
